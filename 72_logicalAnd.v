00d6f6a2846f8553720688b998bcb8ae9d29788c
Amol Salunke <salunkeamol10@gmail.com>
2024-01-30 16:13:50 +0530

    nuances in logical AND operator in two bit operands

--- 72_logicalAnd.v
+++ 72_logicalAnd.v
@@ -2,7 +2,7 @@
 module test;
   initial
     begin
-      $display(2'b10 && 2'bx1); //treat x,z as 0 and check whether it becomes non-zero value or not
+      $display(2'b10 && 2'bx1); //treat x,z as 0 and check whether it becomes non-zero value or not,if both values becomes non-zero then only result will be 1 else x
       $display(2'b01 && 2'bx1);
       $display(2'b10 && 2'b1x);
       $display("--------------------------");
